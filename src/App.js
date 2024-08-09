6f2f8249c4bd974283399a7045934f679f0070a1
amcishara <131766272+amcishara@users.noreply.github.com>
2024-08-09 09:39:38 +0530

    create Login-Signup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import LoginSignup from './components/LogingSignup/LoginSignup';
+
+function App() {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
